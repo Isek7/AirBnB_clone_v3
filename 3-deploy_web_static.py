67908a4195ccefaa5cc23b5d40e7acfe9182f666
Isek7 <bengabrielisek@gmail.com>
2024-03-12 20:40:19 +0100

    Updating IP address

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['100.25.164.216', '54.157.149.145']
 
 
 def do_pack():
